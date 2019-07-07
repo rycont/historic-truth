feae7cf4fb31d93fc97f8e0b34271cc373097fb4
wjdgks1224 <wjdgks1224@gmail.com>
2019-07-07 17:51:11 +0900

    에러 확인

--- tweetComment.mjs
+++ tweetComment.mjs
@@ -1,5 +1,6 @@
 export default (client, status) => new Promise((resolve, reject) => client.post('statuses/update', { status }, (error, tweet, response) => {
     if (error) {
+        console.log(error)
         reject(error)
     }
     resolve(tweet)
